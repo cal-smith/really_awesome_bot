6880e07aa9989a1e4ba6744277a403df5ad6fbe6
Callum Smith <callum1234moo@gmail.com>
2014-04-20 02:39:33 -0400

    now with search!

--- bot.js
+++ bot.js
@@ -3,16 +3,21 @@ var client = new irc.Client('irc.reallyawesomedomain.com', 'yesman',{
 	channels:['#main'],
 });
 
+var chan = "#main"
+
 if ( !String.prototype.contains ) {
     String.prototype.contains = function() {
         return String.prototype.indexOf.apply( this, arguments ) !== -1;
     };
 }
 
-client.addListener('message#main', function(from, message){
+client.addListener('message'+chan, function(from, message){
 	if (message.match(/yesman/i)) {
-		if (message.match(/search/)){
-
+		if (message.search(/\s+search\s+/i) != -1){
+			var split = message.search(/\s+search\s+/i);
+			var search = message.slice(split + 8)
+			search = encodeURIComponent(search);
+			client.say(chan, from+" Searched for: https://www.google.ca/search?q="+search);
 		}
 	}
 	//client.say('#main', "HELLO");
