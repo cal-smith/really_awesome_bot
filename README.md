3ca6173e7878b1c55b61b32a2c8a79ec15cfd8aa
Callum Smith <callum1234moo@gmail.com>
2014-07-21 23:59:42 -0400

    config changes, various functionality updates and improvements

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 YESMAN!
 ==================
 
-Yesman is an IRC bot written in node. The core bot simply depends on [node-irc 0.2.x](https://github.com/martynsmith/node-irc).
+Yesman is an IRC bot written in node. The core bot simply depends on [node-irc](https://github.com/martynsmith/node-irc).
 
-Yesman currently only responds to "yesman <command>".
-
-Yesmans supports "plugins" and "extensions". A plugin is a JSON file that allows for simple call-response style commands. An extension adds to the plugin archtecture, and at its simplest is just a program that prints a string to stdout.
+Yesman supports "plugins" and "extensions". A plugin is a JSON file that allows for simple call-response style commands. An extension adds to the plugin architecture, and at its simplest is just a program that prints a string to stdout. Parsing of the plugin command is done via regex, allowing for partial matching or wildcard style matching.
 
 ##Configuration
 
@@ -13,17 +11,21 @@ There really isn't much to configure. Sample config:
 
 ```json
 {
+	"name":"yesman",
+	"server":"irc.reallyawesomedomain.com",
 	"chan":["#main", "#yesman"],
 	"op":"hansolo669",
 	"windows":true
 }
 ```
 
-- chan: array of channels you want yesman active on.
-- op: users authorized to use advanced yesman features. Additonal security to be added.
-- windows: true|false. There are a couple of things that just work better if we can target Windows. Linux/OS X do not have these issues.
+- name: The username of the bot, and what it will respond to for command purposes.
+- server: The irc server you want to connect to. Currently one server per bot.
+- chan: Array of channels you want yesman active on.
+- op: Users authorized to reload plugins.
+- windows: true|false. Fixes a silly bug that shouldnt happen when running interpreters via command on Windows.
 
-##Writing Plugins
+##Plugins
 
 Plugins are very straight forward, take for example the default help plugin(help.json):
 
@@ -41,15 +43,13 @@ To break things down:
 - response: the command response
 - help: relevent help on the command
 
-When issued `yesman <command>` yesman will respond with the related response text
+see `demo_plugins/` or the bundled `plugins/' directory for some sample plugins.
 
-A usefull help text should be provided with every plugin
+A useful help text should be provided with every plugin.
 
-Additional functionality is planned.
+##extensions
 
-##Writing extensions
-
-Extensions are fairly straight forward as well. They simply add another component and a few more options to the plugin system. Every extentsion comes in two parts: a plugin, and a related script. it is prefferable to keep the plugin and extension naming the similar, if not the same (ex: myext.json, myext.js). Extensions are whole self contained scripts, and are simply run by yesman.
+Extensions are fairly straight forward as well. They simply add another component and a few more options to the plugin system. Every extension comes in two parts: a plugin, and a related script. it is preferable to keep the plugin and extension naming the similar, if not the same (ex: myext.json, myext.js). Extensions are whole self contained scripts/executables with their own dependencies, and are simply run by yesman which captures the output and prints it when the extension has exited.
 
 A sample node script looks like:
 
@@ -70,14 +70,14 @@ console.log("Hello World!");
 
 To break things down:
 
-- command: specifics the command the plugin will respond to
+- command: specifies the command the plugin will respond to
 - type: the type of script we are executing. can be Ruby, Node, Python, or bash scripts [ruby|rb|js|node|javascript|bash|sh|shell|python|py]
 - execute: file to execute
-- help: usefull help text
+- help: useful help text
 
-You can also add a `response` and it will be output before the program executes. This is a good idea on long running programs where the final response may take some time (ie: "response":"Loading...").
+You can also add a `response` and it will output before the program executes. This is a good idea on long running programs where the final response may take some time (ie: "response":"Loading...").
 
-Any output (console.log, puts, printf) will be taken by yesman as input, and output to irc as a result. For example:
+Any output (console.log, puts, printf) will be taken by yesman as a response, and output to irc. For example:
 
 ```ruby
 puts "I am an external Ruby script"
@@ -90,17 +90,14 @@ would print:
 <yesman> yay
 ```
 
-This funcationality allows for all sorts of interesting scripts to be built, go wild!
+This functionality allows for all sorts of interesting scripts to be built, go wild!
 
 ##Help
 
-*Why node-irc 0.2.x?*  
-Because it was installed and I haven't had time to update to the 0.3.x branch yet. I will. Promise!
-
 *An extension isn't running?*  
-Make sure you have the relevent plugin installed and you have run `yesman reload`. If your extension lacks a plugin, either make one, or bug the maintainer untill they do it.
+Make sure you have the relevant plugin installed and you have run `yesman reload`. If your extension lacks a plugin, either make one, or bug the maintainer until they do it.
 
 *THINGS ARE FAILING AND EVERYTHING IS AWFUL*  
 Either the node version you are using is horribly out of date and breaking everything, or *something* I am using is horribly out of date and breaking everything. Post an issue with your node version and what is breaking.
 
-None of the above applicable? Need more immidiate help? want to play with yesman and break him as I'm working on stuff? join irc.reallyawesomedomain.com #main and/or #yesman
+None of the above applicable? Need more immediate help? want to play with yesman and break him as I'm working on stuff? join irc.reallyawesomedomain.com #main and/or #yesman
--- bot.js
+++ bot.js
@@ -2,11 +2,12 @@ var irc = require('irc');
 var exec = require('child_process').exec;
 fs = require('fs');
 var path = require('path');
-var client = new irc.Client('irc.reallyawesomedomain.com', 'yesman',{
-	userName:'yesman',
-	realName:'ReallyAwesomeBot.js',
-	autoConnect: false//so we dont go connecting before we load the conf and break EVERYTHING AAAAAHHHHHHHHHHHHHHHH
-});
+
+if ( !String.prototype.contains ) {//string contains polyfill.
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
+}
 
 //plugin loading here
 var plugins = [];//init plugin global
@@ -23,104 +24,109 @@ function reload(){
 	function loaded(data){
 		plugins.push(data);
 	}
-}
+};
 reload();
 
 var conf = {};//init conf global
-function start(){
+var client;
+function init(){
 	fs.readFile(path.join(__dirname + '/reallyawesome.json'), function(err,data){
 		conf = (JSON.parse(data));
-		console.log(conf.chan)
+		console.log(conf.chan);
+		client = new irc.Client(conf.server, conf.name,{
+			userName:conf.name,
+			realName:'ReallyAwesomeBot.js',
+			autoConnect: false//so we dont go connecting before we load the conf and break EVERYTHING AAAAAHHHHHHHHHHHHHHHH
+		});
 		client.connect(function(){//yes the client joins after the conf is loaded. just to make sure we connect to the right channels
 			for (var i = 0; i < conf.chan.length; i++) {//loops through the chans and connects to each
 				client.join(conf.chan[i]);
 			};
 		});
+		start();
 	});
-}
-start();
+};
+init();
 
-if ( !String.prototype.contains ) {//string contains polyfill. cause contains is prettyer ;3
-    String.prototype.contains = function() {
-        return String.prototype.indexOf.apply( this, arguments ) !== -1;
-    };
-}
-
-client.addListener('message', function(from, channel, message){
-	if (message.match(/^yesman/i)) {
-		if (message.search(/\s+search\s+/i) != -1){
-			var split = message.search(/\s+search\s+/i);
-			var search = message.slice(split + 8);
-			search = encodeURIComponent(search);
-			client.say(channel, from+" Searched for: https://www.google.ca/search?q="+search);
-		}
-		if(message.match(/\s+commands/i)){
-			var response = "Commands: [built in: search, reload] ";
-			for (var i = 0; i < plugins.length; i++) {
-				response += plugins[i].command+", ";
+function start(){
+	client.addListener('message', function(from, channel, message){
+		var namer = "^" + conf.name;
+		namer = new RegExp(namer, "i");
+		if (message.match(namer)) {
+			if (message.search(/\s+search\s+/i) != -1){
+				var split = message.search(/\s+search\s+/i);
+				var search = message.slice(split + 8);
+				search = encodeURIComponent(search);
+				client.say(channel, from+" Searched for: https://www.google.ca/search?q="+search);
 			}
-			client.say(channel, response);
-		}
-		if(message.match(/\s+reload/i)){
-			if (conf.op.contains(from)){//checks if the sender is an allowed op.
-				client.say(channel, "Reloading plugins!");
-				reload();//reloads
+			if(message.match(/\s+commands/i)){
+				var response = "Commands: [built in: search, reload] ";
+				for (var i = 0; i < plugins.length; i++) {
+					response += plugins[i].command+", ";
+				}
+				client.say(channel, response);
 			}
-		}
-		if(message.match(/\s+\w+\s+help/i)){//checks if the help command is precceded by a word and some spaces
-			var help = message.split(" ");//splits on spaces
-			for (var i = 0; i < plugins.length; i++) {//loops through the plugings till it finds the correct one, then outputs its help contents
-				if (plugins[i].command == help[1]){
-					client.say(channel, plugins[i].help);
+			if(message.match(/\s+reload/i)){
+				if (conf.op.contains(from)){//checks if the sender is an allowed op.
+					client.say(channel, "Reloading plugins!");
+					reload();//reloads
 				}
 			}
-		}
-		for (var i = 0; i < plugins.length; i++) {//loops over the plugins, checks if the message matches, and outputs the response.
-			var r = "\\s" + plugins[i].command;
-			var re = new RegExp(r,"i");
-			if (message.match(/\s+\w+\s+help/i)){//stops the help plugin, and other plugins, from emmiting the default help
-				return false;
-			} else if (message.match(re)){
-				if (plugins[i].execute){
-					var type;
-					switch(plugins[i].type){
-						case "ruby":
-						case "rb":
-							if (conf.windows) {
-								type = "ruby.exe";
-							} else{
-								type = "ruby";
-							}
-							break;
-						case "js":
-						case "node":
-						case "javascript":
-							type = "node";
-							break;
-						case "bash":
-						case "sh":
-						case "shell":
-							type = "sh";
-							break;
-						case "python":
-						case "py":
-							type = "python";
-							break;
+			if(message.match(/\s+\w+\s+help/i)){//checks if the help command is precceded by a word and some spaces
+				var help = message.split(" ");//splits on spaces
+				for (var i = 0; i < plugins.length; i++) {//loops through the plugins till it finds the correct one, then outputs its help contents
+					if (plugins[i].command == help[1]){
+						client.say(channel, plugins[i].help);
 					}
-					exec(type + ' extensions/' + plugins[i].execute, function(error, stdout, stderr){
-						console.log(stderr);
-						console.log(stdout);
-						client.say(channel, stdout);
-					});
 				}
-				console.log(message.match(re));
-				client.say(channel, plugins[i].response);
 			}
+			for (var i = 0; i < plugins.length; i++) {//loops over the plugins, checks if the message matches, and outputs the response.
+				var r = "\\s" + plugins[i].command;
+				var re = new RegExp(r,"i");
+				if (message.match(/\s+\w+\s+help/i)){//stops the help plugin, and other plugins, from emmiting the default help
+					return false;
+				} else if (message.match(re)){//regular expressions ftw! we can match anything including * (wildcard) commands ... cause its regexp
+					if (plugins[i].execute){
+						var type;
+						switch(plugins[i].type){
+							case "ruby":
+							case "rb":
+								if (conf.windows) {
+									type = "ruby.exe";
+								} else{
+									type = "ruby";
+								}
+								break;
+							case "js":
+							case "node":
+							case "javascript":
+								type = "node";
+								break;
+							case "bash":
+							case "sh":
+							case "shell":
+								type = "sh";
+								break;
+							case "python":
+							case "py":
+								type = "python";
+								break;
+						}
+						exec(type + ' extensions/' + plugins[i].execute, function(error, stdout, stderr){
+							console.log(stderr);
+							console.log(stdout);
+							client.say(channel, stdout);
+						});
+					}
+					console.log(message.match(re));
+					client.say(channel, plugins[i].response);
+				}
+			}
+			console.log(message);
 		}
-		console.log(message);
-	}
-});
+	});
 
-client.addListener('error', function(message) {
-    console.log('error: ', message);
-});
+	client.addListener('error', function(message) {
+	    console.log('error: ', message);
+	});
+};
